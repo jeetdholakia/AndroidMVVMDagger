a51cbe28c6d38c628e648042aae4877b9af15ca6
Jeet Dholakia <onemediasoft@gmail.com>
2020-05-25 12:30:16 +0530

    Added Glide Module
    Added Glide Module

--- app/src/main/java/dev/jeetdholakia/androidmvvmdagger/di/AppModule.kt
+++ app/src/main/java/dev/jeetdholakia/androidmvvmdagger/di/AppModule.kt
@@ -1,15 +1,35 @@
 package dev.jeetdholakia.androidmvvmdagger.di
 
+import android.app.Application
+import android.graphics.drawable.Drawable
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
+import com.bumptech.glide.request.RequestOptions
 import dagger.Module
 import dagger.Provides
+import dev.jeetdholakia.androidmvvmdagger.R
 
 @Module
 class AppModule {
 
     companion object {
         @Provides
-        fun someString(): String {
-            return "Ola amigo"
+        fun requestOptions(): RequestOptions {
+            return RequestOptions().
+            placeholder(R.drawable.white_background).
+            error(R.drawable.white_background)
+        }
+
+        @Provides
+        fun provideGlideInstance(application: Application, requestOptions: RequestOptions): RequestManager {
+            return Glide.with(application)
+                .setDefaultRequestOptions(requestOptions)
+        }
+
+        @Provides
+        fun provideAppDrawable(application: Application): Drawable {
+            return ContextCompat.getDrawable(application, R.drawable.logo)!!
         }
     }
 }
--- app/src/main/java/dev/jeetdholakia/androidmvvmdagger/presentation/AuthActivity.kt
+++ app/src/main/java/dev/jeetdholakia/androidmvvmdagger/presentation/AuthActivity.kt
@@ -1,8 +1,11 @@
 package dev.jeetdholakia.androidmvvmdagger.presentation
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import com.bumptech.glide.RequestManager
 import dagger.android.support.DaggerAppCompatActivity
 import dev.jeetdholakia.androidmvvmdagger.R
 import javax.inject.Inject
@@ -10,13 +13,18 @@ import javax.inject.Inject
 class AuthActivity : DaggerAppCompatActivity() {
 
     @Inject
-    lateinit var superString: String
+    lateinit var appLogo: Drawable
+
+    @Inject
+    lateinit var requestManager: RequestManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+        setLogo()
+    }
 
-        Log.d("TAG", superString)
-
+    private fun setLogo() {
+        requestManager.load(appLogo).into(findViewById<ImageView>(R.id.login_logo))
     }
 }
